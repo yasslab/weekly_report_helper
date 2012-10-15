3750745c589474853ee2fc201d696333cbc2438e
Yohei YASUKAWA <yasulab@gmail.com>
2012-10-16 00:30:52 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ruby gitlog_getter.rb > /tmp/gitlog_getter.log
-
-
 echo "Your Commit for This Week below"
 echo "==============================="
 cat /tmp/gitlog_getter.log
+
 echo "\n------------------------------"
+
 ruby gcal_getter.rb > /tmp/gcal_getter.log
 cat /tmp/gcal_getter.log
 echo "================================="
